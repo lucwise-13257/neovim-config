109192f06733ea6f44ced1f9baa75660307df7b1
lucwise-13257 <luca_lise@icloud.com>
2024-07-17 19:19:58 -0700

    Clean up lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "tsserver", "tailwindcss" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -14,18 +14,6 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-lspconfig.tailwindcss.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
 require("luau-lsp").setup {
   platform = {
     type = "roblox",
